cc84e917f364ef04533df25e69b84fcdc9ef7761
Billy Ngo <shinlai@Billys-MacBook-Pro-2.local>
2025-02-25 16:27:16 -0600

    Update manage.py to listen to correct port

--- backend/manage.py
+++ backend/manage.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import socket
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+    if "runserver" in sys.argv:
+        # This sets the port to the environment variable PORT (provided by Azure)
+        port = os.getenv("PORT", 8000)  # Default to 8000 if no environment variable is set
+        sys.argv.append(f"0.0.0.0:{port}")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
